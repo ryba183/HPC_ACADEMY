ca185cf038ca526ffa847914f93133872dc0a7f2
ryba183 <pz405506@icm.edu.pl>
2019-09-06 23:49:25 +0200

    First codes

---
+++ float.c
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+	float c = 1e34;
+	std::cout<< c << std::endl;
+	int b = c;
+	std::cout << b << std::endl;
+	
+	
+	
+	
+	
+	return 0;
+}
+
